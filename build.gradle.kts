133af0955b2fb74ee1aa447675349f279a319b94
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-05-28 20:42:29 +0200

    Removing the unused testing capabilities and fixing the package name

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,17 +12,12 @@ repositories {
 val exposedVersion = "0.50.1"
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.xerial:sqlite-jdbc:3.44.1.0")
     implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
     implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 kotlin {
