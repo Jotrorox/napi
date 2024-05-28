fd0c8fff3470bac79ed3ae395eee5ba320c87dad
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-05-28 19:56:12 +0200

    Removing GraalVM

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.23"
-    id("org.graalvm.buildtools.native") version "0.10.2"
 }
 
 group = "com.jotrorox"
@@ -10,15 +9,15 @@ repositories {
     mavenCentral()
 }
 
-val exposed_version = "0.50.1"
+val exposedVersion = "0.50.1"
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("org.xerial:sqlite-jdbc:3.44.1.0")
-    implementation("org.jetbrains.exposed", "exposed-core", exposed_version)
-    implementation("org.jetbrains.exposed", "exposed-dao", exposed_version)
-    implementation("org.jetbrains.exposed", "exposed-jdbc", exposed_version)
+    implementation("org.jetbrains.exposed", "exposed-core", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
+    implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
     implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.6")
 }
 
@@ -28,20 +27,4 @@ tasks.test {
 
 kotlin {
     jvmToolchain(21)
-}
-
-graalvmNative {
-    binaries {
-        named("main") {
-            imageName.set("my-app")
-            mainClass.set("org.jackup.Runner")
-            buildArgs.add("-O4")
-        }
-        named("test") {
-            buildArgs.add("-O0")
-        }
-    }
-    binaries.all {
-        buildArgs.add("")
-    }
 }
